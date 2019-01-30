baeb66761fcfc635076501f38b8ef16d4f45fb20
coturiv <coturiv@gmail.com>
2019-01-30 12:50:34 +0800

    Fix a typo

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -10,7 +10,7 @@ replace() {
 # ==========================
 #
 # WARNING in ./node_modules/typeorm/browser/driver/react-native/ReactNativeDriver.js
-# Module not found: Error: Can't resolve 'react-native-sqlite-storage' in 'F:\Works\Velosum\vcitemobile\node
+# Module not found: Error: Can't resolve 'react-native-sqlite-storage' in '.\node
 # _modules\typeorm\browser\driver\react-native'
 
 replace 'this.sqlite = require("react-native-sqlite-storage");' '' 'node_modules/typeorm/browser/driver/react-native/ReactNativeDriver.js'
